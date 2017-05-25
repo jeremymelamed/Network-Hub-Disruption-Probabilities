6a236a10eff5be27c45101af056eb7d237296726
Jeremy Melamed <jeremymelamed@gmail.com>
2017-05-25 15:58:14 -0700

    binary indicators for replications

--- combination.py
+++ combination.py
@@ -1,31 +1,39 @@
-from math import factorial
 import numpy as np
+import random
 
-class Combination(object):
-    # Return potential number of combinations between two numbers
-    def __init__(self, n=0, h=0, i=0, j=0):
-        self.n = n
-        self.h = h
-        self.i = i
-        self.j = j
+def rep_ids(N, H, reps):
 
-    def combination(self):
-        numerator=factorial(self.n)
-        denominator=(factorial(self.h)*factorial(self.n - self.h))
-        answer=numerator/denominator
-        return answer
+    # Initialize replication matrix
+    combos = np.zeros((N, int(reps)))
+    
+    for i in range(reps):
+        # Randomly sample indexes that could be assigned as hubs for replication
+        foo = random.sample(range(N), H)
+        if i == 0:
+            ids = foo
+        else:
+            ids = np.row_stack((ids, foo))
 
+            # Check that replication is unique
+            test=True
+            while test:
+                test = False
+                ids.sort()
+                for k in range(i-1):
+                    if all(ids[k] == ids[i]):
+                        # If not unique, select new random sample
+                        test = True
+                        ids[i] = random.sample(range(N), H)
 
-    def column_ids(self):
-        col = np.zeros((self.n, 1))
-        for j in range(self.n):
-            if self.i <= self.h*2:
-                if j == 0:
-                    col[j,0] = 1
-                elif self.i + j == self.h:
-                    col[j,0] = 1
-                else:
-                    col[j,0] = 0
-
-        return col
+    # Store binary indicators for potential hub indexes for replication
+    for i in range(reps):
+        for j in range(N):
+            if j in ids[i]:
+                combos[j,i] = 1
+            else:
+                combos[j,i] = 0         
         
+    return combos
+
+
+
