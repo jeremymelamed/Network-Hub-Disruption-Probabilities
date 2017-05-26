73ec5e1da9ae9801beb7a74aac3d3609a28ac5a1
Jeremy Melamed <jeremymelamed@gmail.com>
2017-05-26 07:55:38 -0700

    link new node to closest existing node

--- generate_network.py
+++ generate_network.py
@@ -8,9 +8,10 @@ from combination import rep_ids
 
 np.set_printoptions(suppress=True)
 
-# Create matrix with node ids, location, cost, and replications
+# Function to create node, link, and load matrices for hub and spoke network
 def generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
-                     h_cost_min, h_cost_max, p_min, p_max, link_density):
+                     h_cost_min, h_cost_max, p_min, p_max, link_density,
+                     num_loads, load_min, load_max):
     
     # Check to see if clusters are desired. If so, store centroids.
     if num_clust != 0:
@@ -49,10 +50,16 @@ def generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
                 new_link = [i+1, 2, 1, dist]
                 links = np.row_stack((links, new_link))
             elif i > 1:
-                # Connect to most recent node in unclustered case to avoid overlinking to first nodes
-                orig = nodes[i-1,0]
+                # Compute distances from new node to existing nodes
+                node_dists = []
+                for x in range(i-1):
+                    foo = sqrt((nodes[i,2] - nodes[x,2])**2 + (nodes[i,3] - nodes[x,3])**2)
+                    node_dists.append(foo)
+                # Determine nearest existing node to connect to
+                min_dist = np.argmin(node_dists)                    
+                orig = nodes[min_dist,0]
                 dest = nodes[i,0] 
-                dist = sqrt((nodes[i,2] - nodes[i-1,2])**2 + (nodes[i,3] - nodes[i-1,3])**2)
+                dist = node_dists[min_dist]
                 # Add link going in both directions to ensure connectivity
                 index = len(links) + 1
                 link1 = [index, orig, dest, dist]
@@ -73,8 +80,7 @@ def generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
 
             elif math.ceil(i/clust_nodes) <= num_clust:
                 # Determine which cluster to assign node to.
-                clust_id = math.ceil(i/clust_nodes)
-                
+                clust_id = math.ceil(i/clust_nodes)                
                 theta = random.uniform(0, 2*math.pi)
                 x_cord = centroids[clust_id-1,0] + clust_rad*math.cos(theta)
                 y_cord = centroids[clust_id-1,1] + clust_rad*math.sin(theta)
@@ -102,10 +108,16 @@ def generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
                 links = np.row_stack((links, new_link))
                 
             elif i > 1:
-                # Connect to most recent node in unclustered case to avoid overlinking to first nodes
-                orig = nodes[i-1,0]
+                # Compute distances from new node to existing nodes
+                node_dists = []
+                for x in range(i-1):
+                    foo = sqrt((nodes[i,2] - nodes[x,2])**2 + (nodes[i,3] - nodes[x,3])**2)
+                    node_dists.append(foo)
+                # Determine nearest existing node to connect to
+                min_dist = np.argmin(node_dists)                    
+                orig = nodes[min_dist,0]
                 dest = nodes[i,0] 
-                dist = sqrt((nodes[i,2] - nodes[i-1,2])**2 + (nodes[i,3] - nodes[i-1,3])**2)
+                dist = node_dists[min_dist]
                 # Add link going in both directions to ensure connectivity
                 index = len(links) + 1
                 link1 = [index, orig, dest, dist]
@@ -138,40 +150,51 @@ def generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
     reps = rep_ids(N, H, reps)
     nodes = np.column_stack((nodes, reps))
 
-    return nodes, links
+    # Generate loads matrix: id, origin, destination, load amount
+    for i in range(int(num_loads)):
+        orig = np.random.randint(1, N+1)
+        not_orig = [x for x in range(1, N+1) if x != orig]
+        dest = np.random.choice(not_orig, 1)
+        amount = np.random.uniform(load_min, load_max)
+        if i == 0:
+            loads = [i+1, orig, dest, amount]
+        else:
+            new_load = [i+1, orig, dest, amount]
+            loads = np.row_stack((loads, new_load))        
 
-nodes, links = generate_network(L=100, W=100, N=30, H=4, reps=5, num_clust=5, clust_rad=30,
+    return nodes, links, loads
+
+nodes, links, loads = generate_network(L=500, W=300, N=50, H=4, reps=5, num_clust=10, clust_rad=50,
                          clust_node_perc=.3, h_cost_min=1, h_cost_max=2,
-                         p_min=.1, p_max=.2, link_density = .1)
+                         p_min=.1, p_max=.2, link_density = .1,
+                         num_loads=10, load_min=100, load_max=200)
 
-print(nodes)
-print(links)
+##print(nodes)
+##print(links)
+##print(loads)
 
 
-
-# Create network visualization
-import matplotlib.pyplot as plt
-import networkx as nx
-from collections import defaultdict
-
-# Create array of link tuples
+##### Create network visualization
+##import matplotlib.pyplot as plt
+##import networkx as nx
+##from collections import defaultdict
+##
+### Create array of link tuples
 ##edges = zip(list(links[:,1]), list(links[:,2]))
 ##edges = list(edges)
 ##
 ### Create node location dictionary
 ##locs = defaultdict(list)
-##for x in range(30):
+##for x in range(50):
 ##    locs[x+1].append((nodes[x,2],nodes[x,3]))
-##
 ##locs=dict(locs)
-##print(locs)
-##
 ##
 ##G=nx.Graph()
 ##G.add_edges_from(list(edges))
 ##fix_nodes = locs.keys()
-##pos = nx.spring_layout(G, pos=locs, fixed=fix_nodes)
-##nx.draw_networkx(G, pos)
+##pos = nx.spring_layout(G, pos=locs, fixed=fix_nodes, iterations=0)
+##plt.axis([-10, 510, -10, 310])
+##nx.draw_networkx(G, pos, with_labels=False, node_size=100, alpha=.5)
 ##plt.show()
 
 
