228f6c1a649218587688a4586dd6048c52152b09
Jeremy Melamed <jeremymelamed@gmail.com>
2017-05-26 07:56:47 -0700

    save network plot visualization

--- main.py
+++ main.py
@@ -22,20 +22,32 @@ h_cost_max=float(Params[6][(Params[6].index('=')+1):len(Params[6])])
 p_min=float(Params[7][(Params[7].index('=')+1):len(Params[7])])
 p_max=float(Params[8][(Params[8].index('=')+1):len(Params[8])])
 link_dens=float(Params[9][(Params[9].index('=')+1):len(Params[9])])
-load_dens=float(Params[10][(Params[10].index('=')+1):len(Params[10])])
-num_clust=int(Params[11][(Params[11].index('=')+1):len(Params[11])])
-clust_rad=float(Params[12][(Params[12].index('=')+1):len(Params[12])])
-clust_node_perc=float(Params[13][(Params[13].index('=')+1):len(Params[13])])
+num_loads=float(Params[10][(Params[10].index('=')+1):len(Params[10])])
+load_min=int(Params[11][(Params[11].index('=')+1):len(Params[11])])
+load_max=int(Params[12][(Params[12].index('=')+1):len(Params[12])])
+num_clust=int(Params[13][(Params[13].index('=')+1):len(Params[13])])
+clust_rad=float(Params[14][(Params[14].index('=')+1):len(Params[14])])
+clust_node_perc=float(Params[15][(Params[15].index('=')+1):len(Params[15])])
 
 
 # Create nodes, links, and loads for network
-nodes, links = generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
-                                h_cost_min, h_cost_max, p_min, p_max, link_dens)
+nodes, links, loads = generate_network(L, W, N, H, reps, num_clust, clust_rad, clust_node_perc,
+                                h_cost_min, h_cost_max, p_min, p_max, link_dens,
+                                num_loads, load_min, load_max)
 
-print(nodes)
-print(links)
+        
+### Write files in new folder
+curr_path = path.replace("\\", "\\\\")
+dt = datetime.datetime.now()
+new_path = curr_path + "\\\\" + "Nodes" + str(N) + "_Hubs" + str(H) + '_' + str(dt.strftime("%m.%d.%Y %H.%M"))
+os.makedirs(new_path)
 
-### Create network plot
+os.chdir(new_path)
+np.savetxt('nodes.txt', nodes, fmt='%1.3f', delimiter=',', newline='\r\n')
+np.savetxt('links.txt', links, fmt='%1.3f', delimiter=',', newline='\r\n')
+np.savetxt('loads.txt', loads, fmt='%1.3f', delimiter=',', newline='\r\n')
+
+### Create network visualization
 import matplotlib.pyplot as plt
 import networkx as nx
 from collections import defaultdict
@@ -48,27 +60,16 @@ edges = list(edges)
 locs = defaultdict(list)
 for x in range(N):
     locs[x+1].append((nodes[x,2],nodes[x,3]))
-
 locs=dict(locs)
 
 G=nx.Graph()
 G.add_edges_from(list(edges))
 fix_nodes = locs.keys()
-pos = nx.spring_layout(G, pos=locs, fixed=fix_nodes)
-nx.draw_networkx(G, pos)
-plt.show()
-
-        
-### Write files in new folder
-curr_path = path.replace("\\", "\\\\")
-dt = datetime.datetime.now()
-new_path = curr_path + "\\\\" + "Nodes" + str(N) + "_Hubs" + str(H) + '_' + str(dt.strftime("%m.%d.%Y %H.%M"))
-os.makedirs(new_path)
-
-os.chdir(new_path)
-np.savetxt('nodes.txt', nodes, fmt='%1.3f', delimiter=',', newline='\r\n')
-np.savetxt('links.txt', links, fmt='%1.3f', delimiter=',', newline='\r\n')
-
+pos = nx.spring_layout(G, pos=locs, fixed=fix_nodes, iterations=0)
+plt.axis([-10, L+10, -10, W+10])
+fig = nx.draw_networkx(G, pos, with_labels=False, node_size=75, node_color = 'b', alpha=.5)
+plt.savefig(new_path + '\\network_plot.png')
+plt.show(fig)
 
 
 
